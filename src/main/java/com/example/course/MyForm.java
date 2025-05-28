726592dd328673348c992da5dd696f5a1e97d994
Lydia <lida_zhuikova@mail.ru>
2025-05-29 03:55:40 +0400

    refactor: code was refactored

--- src/main/java/com/example/course/MyForm.java
+++ src/main/java/com/example/course/MyForm.java
@@ -106,9 +106,9 @@ public class MyForm extends Application {
 
     private void setupTransformationMatrix() {
         double mnX = -1, mxX = 1, mnY = -1, mxY = 1, mnZ = -1, mxZ = 1;
-        Matrix viewport = Matrix.makeViewportMatrix(250, -200, 1280, 760, mxZ - mnZ);
+        Matrix viewport = Matrix.makeViewportMatrix(360, 210, 1280, 760, mxZ - mnZ);
         Matrix projection = Matrix.makeMatrixProjection(mnX, mxX, mnY, mxY, mxZ, mnZ);
-        Vector cameraPos = new Vector(0.6, 0.4, 0.8);
+        Vector cameraPos = new Vector(0.6, -0.4, 0.8);
         Vector center = new Vector(0, 0, 0);
         Vector zAxis = cameraPos.subtract(center).normalize();
         Vector xAxis = Vector.vectorMultiplication(new Vector(0, 1, 0), zAxis).normalize();
@@ -117,6 +117,7 @@ public class MyForm extends Application {
         Matrix scale = Matrix.createScaleMatrix(0.2, 0.5, 0.2);
         transformationMatrix = viewport.multiply(projection).multiply(lookAt).multiply(scale);
     }
+
 
     private void applyTransformations() {
         Matrix rotation = Matrix.createYSpinMatrix(rotationAngle);
